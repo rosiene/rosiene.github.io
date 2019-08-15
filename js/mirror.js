91248ceda972cbed06a6edceb05790d5f84def97
Rosiene <rosiene.dilly@gmail.com>
2019-08-15 10:39:59 +0200

    Update mirror.js

--- js/mirror.js
+++ js/mirror.js
@@ -3,5 +3,19 @@ document.getElementById("urlSend").addEventListener("click", function(event){
   var str = document.getElementById("urlValue").value;
   var matches = str.match(/v=([^&]*)/);
   console.log(str);
-  console.log(matches);
+  console.log(matches[1]);
+  if (typeof matches[1] === 'undefined' || matches[1] === null) {
+    alert('Infelizmente não foi possível exibir o vídeo. Tente outro link conforme exemplo')
+  }else{
+    var iframeUrl = "https://www.youtube.com/embed/" + matches[1];
+    var ifrm = document.createElement("iframe");
+    ifrm.setAttribute("src", iframeUrl);
+    ifrm.width = "480px";
+    ifrm.height = "270px";
+    ifrm.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+    ifrm.allowfullscreen =  "";
+    ifrm.frameborder = "0" ;
+    ifrm.style = "-moz-transform: rotatey(180deg); -webkit-transform: rotatey(180deg); transform: rotatey(180deg);";
+    document.getElementById("iframeAqui").appendChild(ifrm);
+  }
 });
