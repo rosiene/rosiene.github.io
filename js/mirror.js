88cf96ba18cc33071d1247cf96ee1a140acd9256
Rosiene <rosiene.dilly@gmail.com>
2019-08-15 11:03:31 +0200

    Update mirror.js

--- js/mirror.js
+++ js/mirror.js
@@ -27,11 +27,11 @@ document.getElementById("urlSend").addEventListener("click", function(event){
           var iframeUrl = "https://www.youtube.com/embed/" + matches[1];
           var ifrm = document.createElement("iframe");
           ifrm.setAttribute("src", iframeUrl);
-          ifrm.width = "480px";
-          ifrm.height = "270px";
+          ifrm.width = 480;
+          ifrm.height = 270;
+          ifrm.frameborder = 0 ;
           ifrm.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
           ifrm.allowfullscreen =  "";
-          ifrm.frameborder = "0" ;
           ifrm.style = "-moz-transform: rotatey(180deg); -webkit-transform: rotatey(180deg); transform: rotatey(180deg);";
           document.getElementById("iframeAqui").innerHTML = "";
           document.getElementById("iframeAqui").appendChild(ifrm);
