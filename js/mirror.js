75d93cab9b9947056cc7cdc0e1bea143589b9433
Rosiene <rosiene.dilly@gmail.com>
2019-08-15 10:27:01 +0200

    Update mirror.js

--- js/mirror.js
+++ js/mirror.js
@@ -2,6 +2,6 @@ document.getElementById("urlSend").addEventListener("click", function(event){
   event.preventDefault();
   var str = document.getElementById("urlValue").value;
   var matches = str.match(/v=([^&]*)/);
-  console.print(str);
-  console.print(matches);
+  console.log(str);
+  console.log(matches);
 });
