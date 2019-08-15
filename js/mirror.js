73053bf2a530d645a368b019412a6c73cffd9f23
Rosiene <rosiene.dilly@gmail.com>
2019-08-15 10:54:10 +0200

    Update mirror.js

--- js/mirror.js
+++ js/mirror.js
@@ -5,15 +5,23 @@ document.getElementById("urlSend").addEventListener("click", function(event){
   
   if (typeof matches === 'undefined' || matches === null) {
       console.log("Erro");
-      var textnode = document.createTextNode("Infelizmente não foi possível exibir o vídeo.<br/>Tente outro link conforme exemplo!");
+      var p1 = document.createElement("p");
+      var p2 = document.createElement("p");
+      p1.innerText("Infelizmente não foi possível exibir o vídeo.");
+      p2.innerText("Tente outro link conforme exemplo!");
       document.getElementById("iframeAqui").innerHTML = "";
-      document.getElementById("iframeAqui").appendChild(textnode);
+      document.getElementById("iframeAqui").appendChild(p1);
+      document.getElementById("iframeAqui").appendChild(p2);
   } else {
       if (typeof matches[1] === 'undefined' || matches[1] === null) {
           console.log("Erro");
-          var textnode = document.createTextNode("Infelizmente não foi possível exibir o vídeo.<br/>Tente outro link conforme exemplo!");
+          var p1 = document.createElement("p");
+          var p2 = document.createElement("p");
+          p1.innerText("Infelizmente não foi possível exibir o vídeo.");
+          p2.innerText("Tente outro link conforme exemplo!");
           document.getElementById("iframeAqui").innerHTML = "";
-          document.getElementById("iframeAqui").appendChild(textnode);
+          document.getElementById("iframeAqui").appendChild(p1);
+          document.getElementById("iframeAqui").appendChild(p2);
       }else{
           console.log("Ok");
           var iframeUrl = "https://www.youtube.com/embed/" + matches[1];
