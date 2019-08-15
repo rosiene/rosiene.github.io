17f4084a3b7dd7b4e7644cceee45ce88e1638688
Rosiene <rosiene.dilly@gmail.com>
2019-08-15 10:49:17 +0200

    Update mirror.js

--- js/mirror.js
+++ js/mirror.js
@@ -4,20 +4,29 @@ document.getElementById("urlSend").addEventListener("click", function(event){
   var matches = str.match(/v=([^&]*)/);
   
   if (typeof matches === 'undefined' || matches === null) {
-    console.log("Erro");
-    var textnode = document.createTextNode("Infelizmente não foi possível exibir o vídeo. Tente outro link conforme exemplo");
-    document.getElementById("iframeAqui").appendChild(textnode);
-  }else{
-    console.log("Ok");
-    var iframeUrl = "https://www.youtube.com/embed/" + matches[1];
-    var ifrm = document.createElement("iframe");
-    ifrm.setAttribute("src", iframeUrl);
-    ifrm.width = "480px";
-    ifrm.height = "270px";
-    ifrm.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
-    ifrm.allowfullscreen =  "";
-    ifrm.frameborder = "0" ;
-    ifrm.style = "-moz-transform: rotatey(180deg); -webkit-transform: rotatey(180deg); transform: rotatey(180deg);";
-    document.getElementById("iframeAqui").appendChild(ifrm);
+      console.log("Erro");
+      var textnode = document.createTextNode("Infelizmente não foi possível exibir o vídeo.<br/>Tente outro link conforme exemplo!");
+      document.getElementById("iframeAqui").innerHTML = "";
+      document.getElementById("iframeAqui").appendChild(textnode);
+  } else {
+      if (typeof matches[1] === 'undefined' || matches[1] === null) {
+          console.log("Erro");
+          var textnode = document.createTextNode("Infelizmente não foi possível exibir o vídeo.<br/>Tente outro link conforme exemplo!");
+          document.getElementById("iframeAqui").innerHTML = "";
+          document.getElementById("iframeAqui").appendChild(textnode);
+      }else{
+          console.log("Ok");
+          var iframeUrl = "https://www.youtube.com/embed/" + matches[1];
+          var ifrm = document.createElement("iframe");
+          ifrm.setAttribute("src", iframeUrl);
+          ifrm.width = "480px";
+          ifrm.height = "270px";
+          ifrm.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+          ifrm.allowfullscreen =  "";
+          ifrm.frameborder = "0" ;
+          ifrm.style = "-moz-transform: rotatey(180deg); -webkit-transform: rotatey(180deg); transform: rotatey(180deg);";
+          document.getElementById("iframeAqui").innerHTML = "";
+          document.getElementById("iframeAqui").appendChild(ifrm);
+      }
   }
 });
