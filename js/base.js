78aed6eca37195169322b0ad8ab89becf704a085
Rosiene <rosiene.dilly@gmail.com>
2020-02-04 15:26:47 +0100

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,65 @@
+$( document ).ready(function() {
+  var calcularMantaElement = document.getElementById('calcularManta');
+
+  if(calcularMantaElement != null){
+    calcularMantaElement.addEventListener("click", function(event){
+      event.preventDefault();
+
+      if (document.getElementById("amostraPontos").value == '' ||
+            document.getElementById("ptoMultiplo").value == '' ||
+            document.getElementById("tamanho").value == ''){
+
+        if (document.getElementById("amostraPontos").value == ''){
+          $('.amostra_pontos').css('color','red');
+          $('#amostraPontos').css('border',' 1px solid red');
+        }
+        if (document.getElementById("ptoMultiplo").value == ''){
+          $('.pto_multiplo').css('color','red');
+          $('#ptoMultiplo').css('border',' 1px solid red');
+        }
+        if (document.getElementById("tamanho").value == ''){
+          $('.tamanho').css('color','red');
+          $('#tamanho').css('border',' 1px solid red');
+        }
+
+      }else{
+        $('.amostra_pontos').css('color','#4f4f4f');
+        $('#amostraPontos').css('border',' 1px solid #ddd');
+        $('.pto_multiplo').css('color','#4f4f4f');
+        $('#ptoMultiplo').css('border',' 1px solid #ddd');
+        $('.tamanho').css('color','#4f4f4f');
+        $('#tamanho').css('border',' 1px solid #ddd');
+
+        var amostraPontos = parseInt(document.getElementById("amostraPontos").value);
+        var ptoMultiplo = parseInt(document.getElementById("ptoMultiplo").value);
+        var ptoMais = parseInt(document.getElementById("ptoMais").value);
+        var tamanho = parseInt(document.getElementById("tamanho").value);
+
+        if(isNaN(ptoMais)){
+          ptoMais = 0;
+        }
+
+        var maxPonto = tamanho / 10 * amostraPontos;
+        var multiploTamanho = ptoMultiplo + ptoMais;
+        var nPtoFantasia = 1;
+
+        while (maxPonto > multiploTamanho) {
+          multiploTamanho = multiploTamanho + ptoMultiplo;
+          nPtoFantasia++;
+        }
+        
+        var nProximoMenor = maxPonto - multiploTamanho + ptoMultiplo;
+        var nProximoMaior = multiploTamanho - maxPonto;
+
+        if (nProximoMenor < nProximoMaior){
+          multiploTamanho = multiploTamanho - ptoMultiplo;
+          nPtoFantasia--;
+        }
+        $('.resultado').append('Você deve colocar '
+          + multiploTamanho + ' pontos na agulha, e a repetição do ponto fantasia será de '
+          + nPtoFantasia + ' vezes.');
+
+        }
+      });
+    }
+  });
