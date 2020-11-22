53e4b85f406d790206d8a207aef0696435e32a54
Rosiene <rosiene.dilly@gmail.com>
2020-11-22 16:27:33 +0100

    slider destaque

--- js/script.js
+++ js/script.js
@@ -49,4 +49,26 @@ $( document ).ready(function() {
       }
     });
 
+    //SLIDER
+    var slideIndex = 1;
+
+    if (screen.width < 768){
+      showDivs(slideIndex);
+    }
+
+    function plusDivs(n) {
+      showDivs(slideIndex += n);
+    }
+
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("img-slider");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      x[slideIndex-1].style.display = "block";
+    }
+
 });
